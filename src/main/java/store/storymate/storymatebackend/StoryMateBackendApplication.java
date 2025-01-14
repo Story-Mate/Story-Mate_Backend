fc87ecd298da62d5aa45c68b79d525424fe04cf5
dongkyunKim <kyun010713@gmail.com>
2025-01-14 16:48:10 +0900

    Chore(#1): 프로젝트 생성 및 설정 파일 구성

---
+++ src/main/java/store/storymate/storymatebackend/StoryMateBackendApplication.java
@@ -0,0 +1,13 @@
+package store.storymate.storymatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryMateBackendApplication.class, args);
+	}
+
+}
