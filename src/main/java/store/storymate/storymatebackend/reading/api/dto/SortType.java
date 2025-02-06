1dbbd259e7d0eb5fe90b9ef9fb1bd8581d418684
yebin <dev.devin.kr@gmail.com>
2025-02-07 08:19:48 +0900

    feat(#9): 책 조회 서비스 구현

---
+++ src/main/java/store/storymate/storymatebackend/reading/api/dto/SortType.java
@@ -0,0 +1,25 @@
+package store.storymate.storymatebackend.reading.api.dto;
+
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SortType {
+    LATEST("최신순", "createdAt"),
+    POPULAR("인기순", "viewCount"),
+    RECOMMEND("추천순", "publishedAt"),
+    NONE("없음", "createdAt");
+
+    private final String kor;
+    private final String field;
+
+    public static String getMappedField(String requestedSortType) {
+        return Arrays.stream(values())
+                .filter(sortType -> sortType.name().equalsIgnoreCase(requestedSortType))
+                .findFirst()
+                .map(SortType::getField)
+                .orElse(NONE.getField());
+    }
+}
