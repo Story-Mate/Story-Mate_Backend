0618fb4583490d3178a71287ede2577b28d882ad
yebin <dev.devin.kr@gmail.com>
2025-02-07 08:16:54 +0900

    feat(#9): 책 조회 레포지토리

---
+++ src/main/java/store/storymate/storymatebackend/reading/domain/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package store.storymate.storymatebackend.reading.domain.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.storymate.storymatebackend.reading.domain.Book;
+import store.storymate.storymatebackend.reading.domain.Genre;
+
+public interface BookRepository extends JpaRepository<Book, Long>, BookRepositoryCustom {
+    Page<Book> findByGenre(Genre genre, Pageable pageable);
+}
