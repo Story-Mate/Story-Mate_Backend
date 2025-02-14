7937e4e504408a94ba4b6a5e1da3962591c95979
yebin <dev.devin.kr@gmail.com>
2025-02-14 20:06:14 +0900

    chore(#26): api 패키지 구조 변경

--- src/main/java/store/storymate/storymatebackend/reading/api/BookController.java
+++ src/main/java/store/storymate/storymatebackend/reading/api/BookController.java
@@ -1,4 +1,4 @@
-package store.storymate.storymatebackend.reading.api.controller;
+package store.storymate.storymatebackend.reading.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
