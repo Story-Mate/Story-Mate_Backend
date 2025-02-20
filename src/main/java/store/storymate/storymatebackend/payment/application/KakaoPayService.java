63a4b1a14c0d5f466365d71e40b036d049fc9cdd
dongkyunKim <kyun010713@gmail.com>
2025-02-21 02:08:06 +0900

    refactor(#53): 상품 구매 시 멤버에 메시지 개수 추가

--- src/main/java/store/storymate/storymatebackend/payment/application/KakaoPayService.java
+++ src/main/java/store/storymate/storymatebackend/payment/application/KakaoPayService.java
@@ -1,8 +1,6 @@
 package store.storymate.storymatebackend.payment.application;
 
 import jakarta.transaction.Transactional;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -25,119 +23,131 @@ import store.storymate.storymatebackend.payment.dto.response.KakaoPayApproveResp
 import store.storymate.storymatebackend.payment.dto.response.KakaoPayPrepareResponse;
 import store.storymate.storymatebackend.payment.repository.PaymentRepository;
 
-@Service
+import java.util.Map;
+
 @Slf4j
+@Service
 @RequiredArgsConstructor
 @EnableConfigurationProperties(KakaoPayProperties.class)
 public class KakaoPayService {
     private static final String BASE_URL = "https://open-api.kakaopay.com";
-    private static final String KAKAO_PAY_READY = "/online/v1/payment/ready";
-    private static final String KAKAO_PAY_APPROVE = "/online/v1/payment/approve";
-    private static final String KAKAO_PAY_CANCEL = "/online/v1/payment/cancel";  // TODO: 취소 기능 추후 구현 예정
+    private static final String PAYMENT_READY_ENDPOINT = "/online/v1/payment/ready";
+    private static final String PAYMENT_APPROVE_ENDPOINT = "/online/v1/payment/approve";
+    private static final String PAYMENT_CANCEL_ENDPOINT = "/online/v1/payment/cancel";  // TODO: 취소 기능 추후 구현 예정
+    private static final String CID = "TC0ONETIME";
 
     private final PaymentRepository paymentRepository;
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
-
     private final MemberUtil memberUtil;
     private final KakaoPayProperties kakaoPayProperties;
     private final RestClient restClient;
-    private KakaoPayPrepareResponse kakaoPayPrepareResponse;
-
-    private HttpHeaders getHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "SECRET_KEY " + kakaoPayProperties.secretKey());
-        headers.set("Content-Type", "application/json");
-        return headers;
-    }
 
     @Transactional
     public KakaoPayPrepareResponse kakaoPayReady(KakaoPayPrepareRequest request) {
         try {
-            Product product = productRepository.findById(request.productId())
-                    .orElseThrow(() -> new BadRequestException("해당 상품을 찾을 수 없습니다."));
+            Product product = getProduct(request.productId());
+            Order order = createOrder(product);
+            KakaoPayPrepareResponse prepareResponse = requestKakaoPayPrepare(order, product);
+            savePayment(prepareResponse.tid(), order);
 
-            Order order = Order.builder()
-                    .product(product)
-                    .member(memberUtil.getCurrentMember())
-                    .status(OrderStatus.PENDING)
-                    .build();
-            orderRepository.save(order);
-
-            Map<String, Object> params = getKakaoPayReadyParams(order, product);
-            HttpHeaders headers = getHeaders();
-
-            kakaoPayPrepareResponse = restClient.post()
-                    .uri(BASE_URL + KAKAO_PAY_READY)
-                    .headers(httpHeaders -> httpHeaders.addAll(headers))
-                    .body(params)
-                    .retrieve()
-                    .body(KakaoPayPrepareResponse.class);
-
-            log.info("KakaoPay Ready Response: {}", kakaoPayPrepareResponse);
-
-            Payment payment = Payment.builder()
-                    .tid(kakaoPayPrepareResponse.tid())
-                    .order(order)
-                    .status(PaymentStatus.PENDING)
-                    .build();
-            paymentRepository.save(payment);
-
-            return kakaoPayPrepareResponse;
+            return prepareResponse;
         } catch (Exception e) {
             log.error("KakaoPay ready failed", e);
             throw new BadRequestException("KakaoPay 결제 준비 요청 실패");
         }
     }
 
-    private Map<String, Object> getKakaoPayReadyParams(Order order, Product product) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("cid", "TC0ONETIME");
-        params.put("partner_order_id", order.getId());
-        params.put("partner_user_id", memberUtil.getCurrentMember().getId());
-        params.put("item_name", product.getName());
-        params.put("quantity", 1);
-        params.put("total_amount", product.getPrice());
-        params.put("tax_free_amount", 0);
-        params.put("approval_url", kakaoPayProperties.approvalUrl());
-        params.put("cancel_url", kakaoPayProperties.cancelUrl());
-        params.put("fail_url", kakaoPayProperties.failUrl());
-        return params;
-    }
-
     @Transactional
     public KakaoPayApproveResponse kakaoPayApprove(String pgToken, KakaoPayApproveRequest request) {
-        Payment payment = paymentRepository.findByTid(request.tid())
+        Payment payment = findPaymentByTid(request.tid());
+        Order order = payment.getOrder();
+        KakaoPayApproveResponse response = requestKakaoPayApprove(order, request.tid(), pgToken);
+
+        updatePaymentStatus(payment);
+        order.getMember().addMessageCount(order.getProduct().getMessageCount());
+        return response;
+    }
+
+    private Product getProduct(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new BadRequestException("해당 상품을 찾을 수 없습니다."));
+    }
+
+    private Order createOrder(Product product) {
+        Order order = Order.builder()
+                .product(product)
+                .member(memberUtil.getCurrentMember())
+                .status(OrderStatus.PENDING)
+                .build();
+        return orderRepository.save(order);
+    }
+
+    private KakaoPayPrepareResponse requestKakaoPayPrepare(Order order, Product product) {
+        return restClient.post()
+                .uri(BASE_URL + PAYMENT_READY_ENDPOINT)
+                .headers(this::setKakaoPayHeaders)
+                .body(createPrepareParams(order, product))
+                .retrieve()
+                .body(KakaoPayPrepareResponse.class);
+    }
+
+    private void savePayment(String tid, Order order) {
+        Payment payment = Payment.builder()
+                .tid(tid)
+                .order(order)
+                .status(PaymentStatus.PENDING)
+                .build();
+        paymentRepository.save(payment);
+    }
+
+    private Payment findPaymentByTid(String tid) {
+        return paymentRepository.findByTid(tid)
                 .orElseThrow(() -> new BadRequestException("해당 TID의 결제 정보를 찾을 수 없습니다."));
+    }
 
-        Map<String, Object> params = getKakaoPayApproveParams(payment.getOrder(), request.tid(), pgToken);
-        HttpHeaders headers = getHeaders();
-
-        KakaoPayApproveResponse response = restClient.post()
-                .uri(BASE_URL + KAKAO_PAY_APPROVE)
-                .headers(httpHeaders -> httpHeaders.addAll(headers))
-                .body(params)
+    private KakaoPayApproveResponse requestKakaoPayApprove(Order order, String tid, String pgToken) {
+        return restClient.post()
+                .uri(BASE_URL + PAYMENT_APPROVE_ENDPOINT)
+                .headers(this::setKakaoPayHeaders)
+                .body(createApproveParams(order, tid, pgToken))
                 .retrieve()
                 .body(KakaoPayApproveResponse.class);
+    }
 
-        log.info("KakaoPay Approve Response: {}", response);
-
+    private void updatePaymentStatus(Payment payment) {
         payment.approve();
         payment.getOrder().complete();
         paymentRepository.save(payment);
-
-        return response;
-
     }
 
-    private Map<String, Object> getKakaoPayApproveParams(Order order, String tid, String pgToken) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("cid", "TC0ONETIME");
-        params.put("tid", tid);
-        params.put("partner_order_id", order.getId());
-        params.put("partner_user_id", memberUtil.getCurrentMember().getId());
-        params.put("pg_token", pgToken);
-        return params;
+    private void setKakaoPayHeaders(HttpHeaders headers) {
+        headers.set("Authorization", "SECRET_KEY " + kakaoPayProperties.secretKey());
+        headers.set("Content-Type", "application/json");
+    }
+
+    private Map<String, Object> createPrepareParams(Order order, Product product) {
+        return Map.of(
+                "cid", CID,
+                "partner_order_id", order.getId(),
+                "partner_user_id", memberUtil.getCurrentMember().getId(),
+                "item_name", product.getName(),
+                "quantity", 1,
+                "total_amount", product.getPrice(),
+                "tax_free_amount", 0,
+                "approval_url", kakaoPayProperties.approvalUrl(),
+                "cancel_url", kakaoPayProperties.cancelUrl(),
+                "fail_url", kakaoPayProperties.failUrl()
+        );
+    }
+
+    private Map<String, Object> createApproveParams(Order order, String tid, String pgToken) {
+        return Map.of(
+                "cid", CID,
+                "tid", tid,
+                "partner_order_id", order.getId(),
+                "partner_user_id", memberUtil.getCurrentMember().getId(),
+                "pg_token", pgToken
+        );
     }
 }
-
